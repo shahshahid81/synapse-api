1f9ef76dc394d5a80b3bdd67b504964c3bf1f37a
Shahid Shah <shahshahid2123@gmail.com>
2024-06-10 21:09:30 +0530

    feat: wip

--- docker-scripts/api-shell.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker compose -f docker-compose.local.yml --env-file .env.development.local exec -it api sh
--- docker-scripts/psql.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker compose -f docker-compose.local.yml --env-file .env.development.local exec -it database psql -U synapse-db-admin -d synapse-db
